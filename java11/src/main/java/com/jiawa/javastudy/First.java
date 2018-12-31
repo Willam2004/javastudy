412574bdacd8e72ad4930ad37d2960ed78002478
guanjing.pangj <guanjing.pangj@alibaba-inc.com>
2019-01-01 00:45:14 +0800

    first java11 demo

---
+++ java11/src/main/java/com/jiawa/javastudy/First.java
@@ -0,0 +1,13 @@
+package com.jiawa.javastudy;
+
+/**
+ * First
+ *
+ * @author panguanjing 2019-01-01
+ **/
+public class First {
+    public static void main(String[] args) {
+        var javaStack = "javaStack";
+        System.out.println(javaStack);
+    }
+}
